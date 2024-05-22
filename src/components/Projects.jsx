b2111ca3f9b1a969791f8d0739140d3e71e6f35d
unknown <shivompaudel@gmail.com>
2024-05-22 13:32:46 -0700

    swapped pics for videos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import Movie from '../assets/Kive-Cinemas.png'
-import Hike from '../assets/hike.png'
-import Ultraverse from '../assets/Ultraverse.png'
-import LinkIcon from '@mui/icons-material/Link';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import Movie from '../assets/movie.mp4'
+import Ultraverse from '../assets/ultraverse.mp4'
 
 function Projects() {
   return (
@@ -16,32 +13,13 @@ function Projects() {
             <ul className="project__list">
                 <li className="project" data-aos="fade-up" data-aos-delay="600">
                     <div className="project__wrapper">
-                        <img src={Hike} className="project__img" alt="Kive Cinemas image"/>
-                        <div className="project__wrapper--bg"></div>
-                        <div className="project__description">
-                            <h3 className="project__description--title">
-                                Hike Medical
-                            </h3>
-                            <h4 className="project__description--sub-title">
-                                Html, Tailwind, TypeScript, Next
-                            </h4>
-                            <p className="project__description--para">
-                                Hike Medical is a startup that I worked for as a Frontend Engineer.
-                                I built their website along with the MVP for their web application.
-                            </p>
-                            <div className="project__description--links">
-                                <a  href="https://www.hikemedical.com/" 
-                                    className="project__description--link"
-                                    target="_blank">
-                                    <LinkIcon/>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li className="project" data-aos="fade-up" data-aos-delay="600">
-                    <div className="project__wrapper">
-                        <img src={Movie} className="project__img" alt="Kive Cinemas image"/>
+                        <video 
+                            className='project__video'
+                            src={Movie}
+                            autoPlay
+                            loop
+                            muted
+                        />
                         <div className="project__wrapper--bg"></div>
                         <div className="project__description">
                             <h3 className="project__description--title">
@@ -53,30 +31,20 @@ function Projects() {
                             <p className="project__description--para">
                                 Kive Cinemas is a website created to give any user an
                                 easy and fast place to find movies. My website utilizes The 
-                                Movie DB API which provides a wide variety of movies.
+                                IMDB API which provides a wide variety of movies.
                             </p>
-                            <div className='project__links--wrapper'>
-                                <div className="project__description--links">
-                                    <a  href="https://kive-cinemas.vercel.app/ " 
-                                        className="project__description--link"
-                                        target="_blank">
-                                        <LinkIcon/>
-                                    </a>
-                                </div>
-                                <div className="project__description--links">
-                                    <a  href="https://github.com/ShivomP/Movie-Project" 
-                                        className="project__description--link"
-                                        target="_blank">
-                                        <GitHubIcon/>
-                                    </a>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </li>
                 <li className="project" data-aos="fade-up" data-aos-delay="600">
                     <div className="project__wrapper">
-                        <img src={Ultraverse} className="project__img" alt="Gmail Clone image"/>
+                        <video 
+                            className='project__video'
+                            src={Ultraverse}
+                            autoPlay
+                            loop
+                            muted
+                        />
                         <div className="project__wrapper--bg"></div>
                         <div className="project__description">
                             <h3 className="project__description--title">
@@ -89,24 +57,8 @@ function Projects() {
                                 Ultraverse is an NFT market place website created for users
                                 to buy, sell, and trade their NFTs. This fully responsive website
                                 pulls data from an API, includes library integration for multiple 
-                                features, and includes dynamic routing to different pages.
+                                features, and dynamic routing to different pages.
                             </p>
-                            <div className="project__links--wrapper">
-                                <div className="project__description--links">
-                                    <a href="https://shivom-internship.vercel.app/" 
-                                        className="project__description--link"
-                                        target="_blank">
-                                        <LinkIcon/>
-                                    </a>
-                                </div>
-                                <div className="project__description--links">
-                                    <a  href="https://github.com/ShivomP/shivom-internship" 
-                                        className="project__description--link"
-                                        target="_blank">
-                                        <GitHubIcon/>
-                                    </a>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </li>

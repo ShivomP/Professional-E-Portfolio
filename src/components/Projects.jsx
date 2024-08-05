7863f5d8f2b2221ade24496e3538925af0961a53
Shivom Paudel <shivompaudel@icloud.com>
2024-08-05 14:38:40 -0700

    added pomodoro project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Movie from '../assets/movie.mp4'
 import Ultraverse from '../assets/ultraverse.mp4'
+import Pomodoro from '../assets/Pomodoro.mp4'
 
 function Projects() {
   return (
@@ -33,6 +34,33 @@ function Projects() {
                                 Kive Cinemas is a website created to give any user an
                                 easy and fast place to find movies. My website utilizes The 
                                 IMDB API which provides a wide variety of movies.
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li className="project" data-aos="fade-up" data-aos-delay="600">
+                    <div className="project__wrapper">
+                        <video 
+                            className='project__video'
+                            src={Pomodoro}
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                        />
+                        <div className="project__wrapper--bg"></div>
+                        <div className="project__description">
+                            <h3 className="project__description--title">
+                                Pomodoro Timer
+                            </h3>
+                            <h4 className="project__description--sub-title">
+                                Html, CSS, JavaScript
+                            </h4>
+                            <p className="project__description--para">
+                                Pomodoro Timer is a website that I created to help me study
+                                using the pomodoro technique. Instead of sticking to the standard
+                                25/5 split of studying and break time ratio, this timer allows you
+                                to set custom times for your studying and break periods. 
                             </p>
                         </div>
                     </div>

d362479af7fc954f5876e76f65bd951ef947d050
Shivom Paudel <shivompaudel@icloud.com>
2025-08-02 14:50:14 -0700

    added ogtool project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import Movie from '../assets/movie.mp4'
 import Ultraverse from '../assets/ultraverse.mp4'
 import Intern from '../assets/Intern-Pres.mp4'
+import OGTool from '../assets/OGTool.mp4'
 
 function Projects() {
   return (
@@ -11,6 +12,34 @@ function Projects() {
                 My <b className="blue">Projects</b>
             </h1>
             <ul className="project__list">
+                <li className="project" data-aos="fade-up" data-aos-delay="400">
+                    <div className="project__wrapper">
+                        <video 
+                            className='project__video'
+                            src={OGTool}
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                        />
+                        <div className="project__wrapper--bg"></div>
+                        <div className="project__description">
+                            <h3 className="project__description--title">
+                                AI-Powered Ingestion Pipeline
+                            </h3>
+                            <h4 className="project__description--sub-title">
+                                Python, OpenAI API, Jina Reader, Selenium, BeautifulSoup
+                            </h4>
+                            <p className="project__description--para">
+                                A scalable ingestion pipeline that extracts content 
+                                from blogs, PDFs, and index pages using Python, Selenium, 
+                                and Jina AI. It enriches the data with OpenAI-powered metadata, 
+                                chunks it for LLM training, and outputs it as clean markdown or JSON. 
+                                Built with multithreading for performance and CLI support for flexible usage.
+                            </p>
+                        </div>
+                    </div>
+                </li>
                 <li className="project" data-aos="fade-up" data-aos-delay="400">
                     <div className="project__wrapper">
                         <video 
